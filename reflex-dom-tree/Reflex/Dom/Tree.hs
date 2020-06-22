419697af290a28c8983ac3c5bafa7e0007a2bafa
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2020-06-22 22:11:15 +0100

    clean

--- reflex-dom-tree/Reflex/Dom/Tree.hs
+++ reflex-dom-tree/Reflex/Dom/Tree.hs
@@ -34,80 +34,67 @@ import Reflex.Dom.Core
 
 import Prelude hiding (div)
 
-data T (payload :: *) (children :: [(* -> *) -> * -> *]) (m :: * -> *) (spine :: *) where
-  Leaf :: m a -> T (Identity a) '[] m spine
-  Branch :: spine -> V xs T m spine -> T Void xs m spine
+data T (fruit :: *) (branches :: [(* -> *) -> * -> *]) (m :: * -> *) (sap :: *) where
+  Leaf :: m a -> T (Identity a) '[] m sap
+  Branch :: sap -> V branches T m sap -> T Void branches m sap
 
---type Leaf a = T (Identity a) '[]
---type Branch xs = T Void xs
-
-deriving instance Functor (T payload children m)
-deriving instance Foldable (T payload children m)
-deriving instance Traversable (T payload children m)
+deriving instance Functor (T fruit branches m)
+deriving instance Foldable (T fruit branches m)
+deriving instance Traversable (T fruit branches m)
 
 data V (shape :: [(* -> *) -> * -> *]) (f :: * -> [(* -> *) -> * -> *] -> (* -> *) -> * -> *) (m :: * -> *) a where
   VNil :: V '[] f m a
   VCons :: f t xs m a -> V xss f m a -> V (f t xs : xss) f m a
 
-deriving instance (forall payload xs. Functor (f payload xs m)) => Functor (V l f m)
-deriving instance (forall payload xs. Foldable (f payload xs m)) => Foldable (V l f m)
-deriving instance (forall payload xs. Traversable (f payload xs m)) => Traversable (V l f m)
+deriving instance (forall fruit branches. Functor (f fruit branches m)) => Functor (V shape f m)
+deriving instance (forall fruit branches. Foldable (f fruit branches m)) => Foldable (V shape f m)
+deriving instance (forall fruit branches. Traversable (f fruit branches m)) => Traversable (V shape f m)
 
-mapVT :: (forall payload xs. T payload xs m a -> T payload xs m b) -> V shape T m a -> V shape T m b
-mapVT f = \case
-  VNil -> VNil
-  VCons t xs -> VCons (f t) (mapVT f xs)
-
-traverseVT :: Applicative m => (forall payload xs. T payload xs m a -> m (T payload xs n b)) -> V xss T m a -> m (V xss T n b)
+traverseVT :: Applicative m => (forall fruit xs. T fruit xs m a -> m (T fruit xs n b)) -> V xss T m a -> m (V xss T n b)
 traverseVT f = \case
   VNil -> pure VNil
   VCons t v -> VCons <$> f t <*> traverseVT f v
 
-elTree :: DomBuilder t m => T payload xs m (Text, Map Text Text) -> m (T payload xs Identity (Element EventResult (DomBuilderSpace m) t))
+elTree
+  :: (DomBuilder t m, PostBuild t m)
+  => T fruit xs m (Text, Dynamic t (Map Text Text))
+  -> m (T fruit xs Identity (Element EventResult (DomBuilderSpace m) t))
 elTree = \case
   Leaf x -> Leaf . Identity <$> x
-  Branch (tg, attrs) xs -> do
-    (n, cs) <- elAttr' tg attrs $ traverseVT elTree xs
-    pure $ Branch n cs
+  Branch (tg, attrs) xs -> fmap (uncurry Branch) $ elDynAttr' tg attrs $ traverseVT elTree xs
 
-widget :: m a -> T (Identity a) '[] m spine
+widget :: m a -> T (Identity a) '[] m sap
 widget = Leaf
-
-node :: spine -> V (x ': xs) T m spine -> T Void (x ': xs) m spine
-node = Branch
 
 -- Needed?
 -- {-# COMPLETE Widget #-}
-pattern Widget
-  :: a
-  -> T (Identity a) '[] Identity spine
+pattern Widget :: a -> T (Identity a) '[] Identity sap
 pattern Widget a = Leaf (Identity a)
+
+node :: tag -> attrs -> V (x ': xs) T m (tag, attrs)-> T Void (x ': xs) m (tag, attrs)
+node = curry Branch
 
 -- Needed?
 -- {-# COMPLETE Node #-}
-pattern Node
-  :: spine
-  -> V xs T Identity spine
-  -> T Void xs Identity spine
+pattern Node :: sap -> V xs T Identity sap -> T Void xs Identity sap
 pattern Node node children = Branch node children
 
 -- Needed?
 -- {-# COMPLETE Nil #-}
-pattern Nil
-  :: V '[] T Identity a
+pattern Nil :: V '[] T Identity a
 pattern Nil = VNil
 
 nil :: V '[] T m a
 nil = VNil
 
 -- Mirrors https://developer.mozilla.org/en-US/docs/Web/CSS/Adjacent_sibling_combinator
-pattern (:+) :: T payload xs m a -> V xss T m a -> V (T payload xs : xss) T m a
+pattern (:+) :: T fruit xs m a -> V xss T m a -> V (T fruit xs : xss) T m a
 pattern h :+ t = VCons h t
 infixr 5 :+
 
-examples :: DomBuilder t m => m ()
+examples :: (DomBuilder t m, PostBuild t m) => m ()
 examples = do
-  t <- elTree $ node ("div", mempty) $ widget blank :+ nil
+  t <- elTree $ node "div" mempty $ widget blank :+ nil
 --  let t' = t :: Int
 
   tt <- elTree $ widget blank
@@ -124,13 +111,13 @@ examples = do
   elTree (widget (el "div" blank)) >>= \case
     Widget () -> pure ()
 
-  t1 <- elTree $ node ("div", mempty) $ widget (el "div" blank) :+ nil
+  t1 <- elTree $ node "div" mempty $ widget (el "div" blank) :+ nil
   let Node _div _ = t1
 --  let Widget _ = t1
 
-  t2 <- elTree $ node ("div", mempty)
+  t2 <- elTree $ node "div" mempty
     $ widget (el "img" blank)
-    :+ node ("div", mempty) (widget (el "br" blank) :+ nil)
+    :+ node "div" mempty (widget (el "br" blank) :+ nil)
     :+ nil
 
   let Node _div
@@ -139,7 +126,7 @@ examples = do
 --         :+ Node _ _
          :+ Nil) = t2
 
-  t4 <- elTree $ node ("div", mempty)
+  t4 <- elTree $ node "div" mempty
     $  widget (el "img" blank)
     :+ widget (el "br" $ pure (3 :: Int))
     :+ nil
